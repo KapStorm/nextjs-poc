34903dad032570fd43cbe242018c945e91fa42f8
Sebastian Arellanes <seb02tian20@gmail.com>
2024-06-16 17:57:51 -0600

    Linkedin and github in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+import IconGithub from '@/components/icons/icon-github'
+import IconLinkedin from '@/components/icons/icon-linkedin'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default function Home () {
   return (
     <main className='flex items-center h-full'>
-      <div className='flex mx-auto gap-6'>
-        <Button><Link href="/articulos">Articulos</Link></Button>
-        <Button><Link href="/modelos">Modelos</Link></Button>
-        <Button><Link href="/marcas">Marcas</Link></Button>
+      <div className='mx-auto space-y-4'>
+        <div className='flex gap-6'>
+          <Button><Link href="/articulos">Articulos</Link></Button>
+          <Button><Link href="/modelos">Modelos</Link></Button>
+          <Button><Link href="/marcas">Marcas</Link></Button>
+        </div>
+        <div className='flex'>
+          <div className='flex mx-auto space-x-2'>
+            <Button size='icon' variant='ghost'>
+              <Link href='https://github.com/kapstorm' target='_blank'>
+                <IconGithub className='w-8 h-8' />
+              </Link>
+            </Button>
+            <Button size='icon' variant='ghost'>
+              <Link href='https://www.linkedin.com/in/sebastián-arellanes-5b589720b/' target='_blank'>
+                <IconLinkedin className='w-8 h-8' />
+              </Link>
+            </Button>
+          </div>
+        </div>
       </div>
     </main>
   )
